a8eee15ea0b59208869478066287e34c90b385ae
TierraSharae <44881884+TierraSharae@users.noreply.github.com>
2018-12-08 11:12:06 -0500

    gets user input for api request

--- Geocoder.java
+++ Geocoder.java
@@ -5,14 +5,18 @@ import java.io.InputStreamReader;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.util.Scanner;
 
 public class Geocoder {
 
     public static void main(String[] args) throws IOException {
 
-
-	URL oracle = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=3101+Walnut+St+Philadelphia,+Pa+19104&key=KEYGOESHERE\n" + 
-		"");
+	System.out.println("pls enter an address:");
+	Scanner scan = new Scanner(System.in);
+	String address = scan.nextLine();
+	address = address.replaceAll(" ", "+"); //necessary to build url in api
+	
+	URL oracle = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=" +address+ "&key=" +SecretFile.key);
 	URLConnection yc = oracle.openConnection();
 	BufferedReader in = new BufferedReader(new InputStreamReader(
 		yc.getInputStream()));       
