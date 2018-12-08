d898ffe8a32a214b3240390f637f447464f16daa
TierraSharae <44881884+TierraSharae@users.noreply.github.com>
2018-12-08 16:30:45 -0500

    fixed url encoding with actual URL encoder

--- Geocoder.java
+++ Geocoder.java
@@ -2,20 +2,29 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 import java.net.MalformedURLException;
 import java.net.URL;
 import java.net.URLConnection;
+import java.net.URLEncoder;
 import java.util.Scanner;
+
 
 public class Geocoder {
 
+
     public static void main(String[] args) throws IOException {
 
-	System.out.println("pls enter an address:");
+	System.out.println("please enter an address:");
 	Scanner scan = new Scanner(System.in);
 	String address = scan.nextLine();
-	address = address.replaceAll(" ", "+"); //necessary to build url in api
-	
+	try {
+	    address = URLEncoder.encode(address, "UTF-8");
+	} catch (UnsupportedEncodingException e) {
+	    // TODO Auto-generated catch block
+	    e.printStackTrace();
+	}
+
 	URL oracle = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=" +address+ "&key=" +SecretFile.key);
 	URLConnection yc = oracle.openConnection();
 	BufferedReader in = new BufferedReader(new InputStreamReader(
