ae53bff5f8bb6818ceb8daf0fb9ee5114756d793
TierraSharae <44881884+TierraSharae@users.noreply.github.com>
2018-12-08 19:18:43 -0500

    gets address from user, returns lat/lng

--- Geocoder.java
+++ Geocoder.java
@@ -1,5 +1,7 @@
 
 import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.UnsupportedEncodingException;
@@ -9,11 +11,15 @@ import java.net.URLConnection;
 import java.net.URLEncoder;
 import java.util.Scanner;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class Geocoder {
 
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, JSONException {
+
 
 	System.out.println("please enter an address:");
 	Scanner scan = new Scanner(System.in);
@@ -25,13 +31,32 @@ public class Geocoder {
 	    e.printStackTrace();
 	}
 
+
 	URL oracle = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=" +address+ "&key=" +SecretFile.key);
 	URLConnection yc = oracle.openConnection();
 	BufferedReader in = new BufferedReader(new InputStreamReader(
 		yc.getInputStream()));       
-	String inputLine;       
-	while ((inputLine = in.readLine()) != null)
-	    System.out.println(inputLine);       
+	String inputLine;
+	StringBuffer response = new StringBuffer();
+
+
+	while ((inputLine = in.readLine()) != null) {
+	     response.append(inputLine + "\n");     
+	}
+	String responseString = response.toString();
+	//JSONObject jObj = new JSONObject(responseString);
+	String []locationLat = responseString.split("location");
+	String []latArray = locationLat[1].split("lat\" : ");
+	String []latArray2 = latArray[1].split(",");
+	double lat = Double.valueOf(latArray2[0]);
+	System.out.println(lat);
+	
+	String []locationLng = responseString.split("location");
+	String []lngArray = locationLng[1].split("lng\" : ");
+	String []lngArray2 = lngArray[1].split(" ");
+	double lng = Double.valueOf(lngArray2[0]);
+	System.out.println(lng);
+
 	in.close();
 
 
