54c131a6a245640074b111470fef3ac0b5ff2a53
TierraSharae <44881884+TierraSharae@users.noreply.github.com>
2018-12-10 05:30:37 -0500

    converts user address to zone

--- Geocoder.java
+++ Geocoder.java
@@ -15,9 +15,7 @@ import org.json.JSONObject;
 
 public class Geocoder {
 
-
     public static void main(String[] args) throws IOException, JSONException {
-
 
 	System.out.println("please enter an address:");
 	Scanner scan = new Scanner(System.in);
@@ -28,7 +26,6 @@ public class Geocoder {
 	    // TODO Auto-generated catch block
 	    e.printStackTrace();
 	}
-
 
 	URL oracle = new URL("https://maps.googleapis.com/maps/api/geocode/json?address=" +address+ "&key=" +SecretFile.key);
 	URLConnection yc = oracle.openConnection();
@@ -46,22 +43,47 @@ public class Geocoder {
 	JSONArray results = jObj.getJSONArray("results");
 	JSONObject resultsObj = results.getJSONObject(0);
 	String formatted_address = results.getJSONObject(0).getString("formatted_address");
-
 	JSONObject geometry = resultsObj.getJSONObject("geometry");
 	JSONObject location = geometry.getJSONObject("location");
 	double lat = location.getDouble("lat");
 	double lng = location.getDouble("lng");
+	int zone = 0;
 
 	System.out.println("lat = " +lat);
 	System.out.println("lng = "+ lng);
-	if(!formatted_address.contains("Philadelphia")){
-	    System.out.println("not in phl");
+
+
+	if (lat < 40.140689 && lat > 40.089990 && lng > -75.071930 && lng < -74.952492){ 
+	    zone =  1;
+	} else if (lat < 40.089990 && lat > 40.032053 && lng > -75.012725 && lng < -74.955568) {
+	    zone = 2;
+	} else if (lat < 40.089990 && lat > 40.032053 && lng > -75.012725 && lng < -75.071930) {
+	    zone = 3;
+	} else if (lat < 40.089990 && lat > 40.032053 && lng > -75.186205 && lng < -75.071930) {
+	    zone = 4;
+	} else if (lat < 40.089990 && lat > 40.032053 && lng > -75.268087 && lng < -75.186205) {
+	    zone = 5;
+	} else if (lat < 40.032053 && lat > 39.963886 && lng > -75.073577 && lng < -75.00477) {
+	    zone = 6;
+	} else if (lat < 40.032053 && lat > 39.963886 && lng > -75.198035 && lng < -75.073577) {
+	    zone = 7;
+	} else if (lat < 40.032053 && lat > 39.963886 && lng > -75.281930 && lng < -75.198035) {
+	    zone = 8;
+	} else if (lat < 39.963886 && lat > 39.869780 && lng > -75.200314 && lng < -75.130312) {
+	    zone = 9;
+	} else if (lat < 39.963886 && lat > 39.869780 && lng > -75.281930 && lng < -75.198035) {
+	    zone = 10; //will change this
+	} else {
+	    System.out.print("Sorry, we do not currently have data for "+formatted_address+". ");
+	    if(!formatted_address.contains("Philadelphia")){
+		System.out.println("Please try a location in Philadelphia.");
+	    }
 	}
+
+	System.out.println("zone = " +zone);
 
 	in.close();
 
     }
 
 }
-
-
